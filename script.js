66623499563215967609176873ef6303cc32ae91
anukul76 <anukulthebest@gmail.com>
2023-03-11 15:58:50 +0530

    2nd last

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Technical Support Engineer", "Developer", "Designer"],
+        strings: ["Technical Support Engineer", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Technical Support Engineer", "Developer", "Designer"],
+        strings: ["Technical Support Engineer", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
