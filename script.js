206552e6ae80d5e290c01ce90cb9a073e81bd75b
anukul76 <anukulthebest@gmail.com>
2023-03-13 01:54:48 +0530

    anukul

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
         
-       
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
 
     // slide-up script
@@ -22,17 +27,22 @@ $(document).ready(function(){
         $('html').css("scrollBehavior", "smooth");
     });
 
-  
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Technical Support Engineer"],
+        strings: ["Techinical Support Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Technical Support Engineer"],
+        strings: ["Techinical Support Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
