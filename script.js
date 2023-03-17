62ea9c12be347a922c58e45080d76798f24b15df
anukul76 <anukulthebest@gmail.com>
2023-03-17 19:33:15 +0530

    anukul

--- script.js
+++ script.js
@@ -7,24 +7,7 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
@@ -35,14 +18,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Tecinical Support Engineer"],
+        strings: ["Technical Support Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Tecinical Support Engineer"],
+        strings: ["Technical Support Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -71,11 +54,3 @@ $(document).ready(function(){
         }
     });
 });
-document.getElementById("res").addEventListener("click",()=>{
-    window.open('https://drive.google.com/file/d/1W-hPjAirkoJ3Be2oHbYd5N21EcOYNCAx/view?usp=share_link', '_blank');
-})
-
-
-document.getElementById("down").addEventListener("click",()=>{
-    window.open('https://drive.google.com/file/d/1W-hPjAirkoJ3Be2oHbYd5N21EcOYNCAx/view?usp=share_link', '_blank');
-})
